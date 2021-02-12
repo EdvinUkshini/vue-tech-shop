4376e4af9006f5ab57eeb4348ffc2fd7ed7f642c
bi46173 <bi46173@ubt-uni.net>
2021-02-12 23:32:16 +0100

    Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,35 +1,93 @@
-<template>
-<nav>
-<div id="logo">TechShop</div>
-<ul>
-<div class="table">
-  <li><router-link to="/">Home</router-link></li>
-  <li><router-link to="/Computers">Computers</router-link></li>
-  <li><router-link to="/Laptops">Laptops</router-link></li>
-  <li class="dropdown">
-    <label for="toggle-dropdown" class="submenu">Parts +</label>
-	<input type="checkbox" id="toggle-dropdown" />
-    <div class="dropdown-content">
-      <router-link to="/Cpu">CPU</router-link>
-      <router-link to="/Ram">RAM</router-link>
-      <router-link to="/Gpu">Graphic Card</router-link>
-	  <router-link to="/Storage">Storage Device</router-link>
-    </div>
-  </li>
-  <li><router-link to="/Peripherals">Peripherals</router-link></li>
-  </div>
-</ul>
-</nav>
-</template>
+     <template>
+      <v-bottom-navigation
+        background-color="black"
+        horizontal
+      >
+        <router-link to="/" class="navItem item">
+          <span>Home</span>
+        </router-link>
+        <router-link to="/Computers" class="navItem item">
+          <span>Computers</span>
+        </router-link>
+        <router-link to="/Laptops" class="navItem item">
+          <span>Laptops</span>
+        </router-link>
+        <v-menu open-on-hover offset-y>
+          <template v-slot:activator="{ on }">
+            <a v-on="on" class="navItem item">
+              <span>Parts</span>
+            </a>
+          </template>
+          <v-card
+            class="mx-auto"
+            max-width="344"
+            outlined
+          >
+
+            <v-list-item
+              v-for="(item, index) in items"
+              :to="item.path"
+              :key="index"
+              href="/"
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+
+          </v-card>
+        </v-menu>
+         <router-link to="/Peripherals" class="navItem item">
+          <span>Peripherals</span>
+        </router-link>
+        <router-link to="/Contact" class="navItem item">
+          <span>Contact Us</span>
+        </router-link>
+      </v-bottom-navigation>     
+     </template>
+     <style>
+     .navItem{
+        display: flex;
+        color:#fff;
+        background-color: transparent;
+            border-radius: 0;
+            box-shadow: none;
+            flex: 0 1 auto;
+            font-size: 0.75rem;
+            height: inherit;
+            max-width: 168px;
+            min-width: 80px;
+            position: relative;
+            text-transform: none;
+            align-items: center;
+            justify-content: center;
+            padding-right:5px;
+            margin-bottom:5px;
+     }
+     .v-application a.item {
+        color: #fff !important;
+        text-align: center;
+        text-decoration: none;
+        font-size:17px;
+        margin:0 5px;
+        border-right:2px solid #00cc6a;
+    }
+    .v-application a:hover {
+        color:#00cc6a !important;
+    }
+     </style>
 <script>
-export default {
-  name: 'Navbar',
-  components: {
-  }
-}
+    export default {
+        name: 'Navbar',
+        data () {
+            return {
+                items: [
+                    { title: 'Cpu', path:'cpu' },
+                    { title: 'Ram', path:'ram'},
+                    { title: 'Gpu', path:'gpu'},
+                    { title: 'Storage', path:'storage' },
+                    { title: 'Motherboard', path:'motherboard'},
+                ],
+            }
+        },
+        
+    }
 </script>
-<style scoped>
-.v-application a {
-color:#fff;
-}
-</style>
