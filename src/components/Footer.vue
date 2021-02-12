7cc11ac320435344a0cfb495898b4e17d42bfd7f
EdvinUkshini <edvinukshini@gmail.com>
2021-02-12 23:18:41 +0100

    Header and Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+        <v-footer
+      :padless="true"
+    >
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="black white--text text-center"
+      >
+        <v-divider></v-divider>
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>TechShop</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+</template>
+<script>
+export default {
+    name:'Navbar'
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+    <template>
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="black"
+      dark
+    >
+      <v-toolbar-title
+        style="width: 350px"
+      >
+        <a href="/" class="white--text" style="text-decoration: none"><v-icon>mdi-cart</v-icon>&nbsp;TechShop</a>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down pl-10 ml-4"
+      />
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+      <v-btn href="/Cart" icon>
+        <v-badge
+          content="2"
+          value="2"
+          color="green"
+          overlap
+        >
+          <v-icon>mdi-cart</v-icon>
+        </v-badge>
+      </v-btn>
+    </v-app-bar>
+    </template>
