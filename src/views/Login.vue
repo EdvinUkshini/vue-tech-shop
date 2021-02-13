b96f75456d33de8e528f0f789b4745e296942274
bi46173 <bi46173@ubt-uni.net>
2021-02-13 23:39:14 +0100

    Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,62 +1,36 @@
 <template>
-    <div>
-        <v-container>
-            <v-row>
-               <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="email"
-            :counter="10"
-            label="Email"
-            required
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            type="password"
-            v-model="password"
-            :counter="10"
-            label="Password"
-            required
-          ></v-text-field>
-        </v-col>
-        <button @click="login">Login</button>
-        <p>Need an account?</p> <router-link to="/signup">Sign Up</router-link>
-            </v-row>
-        </v-container>
-    </div>
+<div>
+    
+        Email:<br>
+        <input type="email" name="email" value required autofocus autocomplete v-model="email"/>
+        <br />Password:<br>
+        <input type="password" name="password" required v-model="password"/>
+        <br />
+        <button @click="Login">Login</button>
+</div>
 </template>
 <script>
-import firebase from "firebase";
-export default {
-    name: "Login",
+import firebase from 'firebase'
+  export default {
     data(){
-        return{
-            email:"",
-            password:""
-        }
+      return{
+        email:"",
+        password:""
+      };
     },
     methods:{
-        login:function(){
-            //Log the user in
-            firebase.auth().signInWithEmailAndPassword(this.email,this.password).then(
-                user => {
-                    console.log(user.data)
-                },
-                err =>{
-                    alert(err);
-                }
-            )
+        Login:function(){
+            firebase
+            .auth()
+            .signInWithEmailAndPassword(this.email,this.password)
+            .then(data=>{
+                this.$router.push({name:"Home",})
+            })
+            .catch(err=>{
+                alert(err);
+            });
         }
+        
     }
-};
+}
 </script>
-<style lang="scss" scoped>
-
-</style>
