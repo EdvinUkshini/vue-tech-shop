2e2d5fd419bf4b3894f9c12dfb287a90db2b35d2
EdvinUkshini <edvinukshini@gmail.com>
2021-02-13 23:50:59 +0100

    Simple if condition of loggedIn and Log Out form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
-    test
+    <template v-if="user.loggedIn">
+        {{user.data.email}}
+        <button @click="signOut">Sign Out</button>
+    </template>
+    <template v-else>
+
+    </template>
   </div>
 </template>
+<script>
+import {mapGetters} from "vuex"
+import firebase from 'firebase'
+export default {
+  computed:{
+    ...mapGetters({
+      user:"user"
+    })
+  },
+  methods:{
+    signOut:function(){
+      firebase
+      .auth()
+      .signOut()
+      .then(()=>{
+        this.$router.replace({
+          name:"Home"
+        })
+      })
+    }
+  }
+  
+}
+</script>
